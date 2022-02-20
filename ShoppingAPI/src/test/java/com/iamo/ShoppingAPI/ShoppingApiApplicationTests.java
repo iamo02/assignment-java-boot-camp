8f9f484c8e891758166174e4dc11260c788f0ebd
Pongchai Boonmee <iamo.java@gmail.com>
2022-02-20 19:16:35 +0700

    BREAKING CHANGE: Create Project ShoppingAPI

---
+++ ShoppingAPI/src/test/java/com/iamo/ShoppingAPI/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamo.ShoppingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
